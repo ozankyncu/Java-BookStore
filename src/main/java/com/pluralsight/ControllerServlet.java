ed6038990780ccbdff403deae38d1ea97b7ce1b7
busramumcu <busramumcu@gmail.com>
2019-03-13 11:09:33 +0300

    Edit Form
    Edit fonksiyonu eklendi.

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -62,6 +62,9 @@ public class ControllerServlet extends HttpServlet {
 				case "/delete":
 					deleteBook(request, response);
 					break;
+				case "/edit":
+					showEditForm(request, response);
+					break;
         default:
 				   listBooks(request, response);
            break;
@@ -112,6 +115,16 @@ public class ControllerServlet extends HttpServlet {
 			int id = Integer.parseInt(request.getParameter("id"));
 			bookDAO.deleteBook(id);
 			response.sendRedirect("list");
+	}
+	
+	private void showEditForm(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, IOException, ServletException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		Book existingBook = bookDAO.getBook(id);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/BookForm.jsp");
+		request.setAttribute("book", existingBook);
+		dispatcher.forward(request,response);
+
 	}
 
 
